0f2cfa174abe0d786950d72891fc2834a567875d
Pavwos <rspavanelli@gmail.com>
2024-04-09 15:00:37 -0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Header } from "./components/header"
 import { Attendee } from "./components/attendee-list"
 
-export function App() {
+export function app() {
   return (
     <div className="max-w-[1216px] mx-auto py-5 flex flex-col gap-5">
       <Header/>
@@ -10,4 +10,4 @@ export function App() {
   )
 }
 
-export default App
+export default app
